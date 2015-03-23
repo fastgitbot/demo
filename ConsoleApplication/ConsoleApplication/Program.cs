47f9b87ff97ba3f39871cabcb9c0266c697a5ad8
Andrey Frolov <frolov.a@shturmann.com>
2015-03-23 14:51:49 +0300

    Commit branch changes

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world 2!");
+            Console.WriteLine("Hello world again!");
             Console.ReadLine();
         }
     }
